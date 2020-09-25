14b36f9c641aaf08480a67c535c39d2bff3a8772
Tobias Davis <accounts@davistobias.com>
2020-09-25 08:51:19 -0500

    fix: the default template requires feeds for now

--- site/_data/metadata.js
+++ site/_data/metadata.js
@@ -7,8 +7,20 @@ https://github.com/an11ty/template
 
 */
 
+const domain = 'example.com'
+
 module.exports = {
 	title: 'An11ty Website',
-	url: `https://example.com/`,
-	description: 'Built with an11ty.com'
+	url: `https://${domain}/`,
+	description: 'Built with an11ty.com',
+	feed: {
+		subtitle: 'An11ty Feed',
+		filename: 'feed.xml',
+		path: '/feed/feed.xml',
+		id: `https://${domain}/`
+	},
+	json_feed: {
+		path: '/feed/feed.json',
+		url: `https://${domain}/feed/feed.json`
+	}
 }
