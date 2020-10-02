688bae4ef42da7288f549a495d97a14b5dd044d5
Mahesh kumar Dey <mdey0108@gmail.com>
2020-10-02 10:31:04 +0530

    Update metadata.js

--- site/_data/metadata.js
+++ site/_data/metadata.js
@@ -13,19 +13,25 @@ module.exports = {
 	title: 'An11ty Website',
 	url: `https://${domain}/`,
 	description: 'Built with an11ty.com',
-	feed: {
+	feed:
+	{
 		subtitle: 'An11ty Feed',
 		filename: 'feed.xml',
 		path: '/feed/feed.xml',
 		id: `https://${domain}/`
 	},
-	json_feed: {
+	json_feed:
+	{
 		path: '/feed/feed.json',
 		url: `https://${domain}/feed/feed.json`
 	},
+	
+	
 	// The base @an11ty/template comes with a few example pages, as well
 	// as some example media files. If you remove this line, the media
 	// files won't be processed. To stop the example pages from rendering,
 	// you will need to look at the file `.an11ty-eleventyignore`
+	
+	
 	renderExampleContent: true
 }
